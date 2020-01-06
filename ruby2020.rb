690723d22ae07c35e8f94688b213b934f704f285
Edwin Calano Paras <ecparas@amazon.com>
2020-01-06 15:49:37 -0500

    1600 06JAN2020

--- ruby2020.rb
+++ ruby2020.rb
@@ -1,7 +1,11 @@
 # --- Lesson 7-First Ruby File --- #
 
-puts "Enter your name: "
-name = gets.chomp
-puts "Enter your age: "
-age = gets.chomp.to_i   # ---converts string to integer--- #
-p "Hello #{name}. You are #{age} years old? Go away, old fart."
+  # puts "Enter your name: "
+  # name = gets.chomp
+  # puts "Enter your age: "
+  #age = gets.chomp.to_i   # ---converts string to integer--- #
+  #p "Hello #{name}. You are #{age} years old? Go away, old fart."
+
+  free_text = "A long time ago, in a galaxy far, far, away..."
+  free_text. length
+  p free_text[3]   # ---index position starts at zero---#
